90c42100d46ee97c0c9f6e7b41dfda943c2597a1
JoroStef <georgy_stefanov@abv.bg>
2022-09-19 07:34:24 +0300

    edit connection string

--- Simple.Api/Simple.Data/SimpleDbContext.cs
+++ Simple.Api/Simple.Data/SimpleDbContext.cs
@@ -13,7 +13,7 @@ namespace Simple.Data
         {
             // @"Server=127.0.0.1;Port=5432;Database=SimpleDb;User Id=postgres;Password=password;"
             // @"Database=simpledb; Data Source=pgserver123456789.postgres.database.azure.com; User Id=myadmin@pgserver123456789; Password=123pG!321"
-            optionsBuilder.UseNpgsql(@"Database=simpledb; Data Source=pgserver123456789.postgres.database.azure.com; User Id=myadmin@pgserver123456789; Password=123pG!321");
+            optionsBuilder.UseNpgsql(@"Database=simpledb; Server=pgserver123456789.postgres.database.azure.com; User Id=myadmin@pgserver123456789; Password=123pG!321; Ssl Mode=Require;");
         }
     }
 }
